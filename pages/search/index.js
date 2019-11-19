ddb9304b2ac5e83046bf676e4b4fc278158bac6c
Chris L <chris@DongyutkiiPhone.guest.corp.microsoft.com>
2019-11-19 10:20:32 +0800

    be able to get data from API

---
+++ pages/search/index.js
@@ -0,0 +1,89 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    cityArray: ['Beijing', 'Shanghai', 'Suzhou'],
+    cityCode: ['108288', '108296', '118163'],
+    interestsArray: ['all', 'music', 'film', 'drama', 'commonweal', 'salon', 'exhibition', 'party', 'sports', 'travel', 'others'],
+    cityIndex: 0,
+    interestsIndex: 0,
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  // city Change Listener
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      cityIndex: e.detail.value
+    })
+  },
+  // interests Change Listener
+  bindPickerInterestsChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      interestsIndex: e.detail.value
+    })
+  },
+  // request data from API
+  getDataFromAPI: function (e) {
+    var baseDoubanURL = "https://douban.uieee.com/v2/event/list?";
+    var realRequestURL = baseDoubanURL + "loc=" + this.data.cityCode[this.data.cityIndex] + "&type=" + this.data.interestsArray[this.data.interestsIndex];
+    console.log(realRequestURL);
+    wx.request({
+      url: realRequestURL,
+      success: function (res) {
+        console.log(res);
+      }
+    })
+    wx.navigateTo({
+      url: '/pages/index/index',
+    })
+  },
+  //事件处理函数
+  getCardView: function () {
+    wx.navigateTo({
+      url: '/pages/index/index',
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
